9c58f664946728af6a67fb3debac1114590c5fac
Paper99 <1398872296@qq.com>
2024-01-20 12:54:19 +0800

    1.1 queryTimeAfter

--- src/main/java/cn/paper_card/player_last_quit/api/PlayerLastQuitApi2.java
+++ src/main/java/cn/paper_card/player_last_quit/api/PlayerLastQuitApi2.java
@@ -3,9 +3,13 @@ package cn.paper_card.player_last_quit.api;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 public interface PlayerLastQuitApi2 {
     boolean addOrUpdateByUuid(@NotNull QuitInfo info) throws Exception;
 
     @Nullable QuitInfo queryByIpLatest(@NotNull String ip) throws Exception;
+
+    @NotNull List<QuitInfo> queryTimeAfter(long time) throws Exception;
 }
